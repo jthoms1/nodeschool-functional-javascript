b40ed548f7ca3a212316ba71e5b8306dc959d158
Josh Thomas <jthoms1@gmail.com>
2016-02-09 19:44:48 -0600

    Completed 17 and 18.

--- 17-currying.js
+++ 17-currying.js
@@ -1,6 +1,14 @@
 'use strict';
 
 function curryN(fn, n) {
+  n = n || fn.length;
+
+  return function(value) {
+    if (n > 1) {
+      return curryN(fn.bind(this, value), n - 1);
+    }
+    return fn(value);
+  };
 }
 
 module.exports = curryN;
